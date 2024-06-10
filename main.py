23c855a2b2575314c10f14370a56bc4d4b385252
Carlos HJ <chuenumanj@gmail.com>
2024-06-09 21:08:24 -0400

    Arreglo

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from Grafos import Grafos
 
 window = tk.Tk() # Crear la ventana
 window.title("Visualizador de Imagenes") # Titulo de la ventana
-altura_window = 720 # Altura de la ventana
-ancho_window = 1280 # Ancho de la ventana
+altura_window = 480 # Altura de la ventana
+ancho_window = 1130 # Ancho de la ventana
 window.geometry(f"{ancho_window}x{altura_window}") # Tamaño de la ventana
 
 # Parte 1 Elementos Tkinter
@@ -41,9 +41,9 @@ entry_doctor = tk.Entry(window)
 entry_doctor.grid(row=0, column=3, padx=10, pady=5)
 
 label_id = tk.Label(window, text="ID:")
-label_id.grid(row=2, column=2, padx=10, pady=5)
+label_id.grid(row=1, column=2, padx=10, pady=5)
 entry_id = tk.Entry(window)
-entry_id.grid(row=2, column=3, padx=10, pady=5)
+entry_id.grid(row=1, column=3, padx=10, pady=5)
 
 # Crear una tabla para mostrar registros de doctores
 treeview_doctores = ttk.Treeview(window, columns=('Nombre', 'ID'))
@@ -82,10 +82,19 @@ grafo = Grafos()
 #Permitiendo agregar, buscar y eliminar registros. 
 #Crear una interfaz para que los usuarios puedan agregar, buscar y eliminar registros de pacientes.
 def agregar():
-    nombre = entry_nombre.get()
-    expediente = int(entry_expediente.get())
-    arbol_pacientes.Agregar(expediente, nombre)
-    actualizar_tabla()
+    if not entry_expediente.get() or not entry_nombre.get():
+            messagebox.showerror("Error", "Por favor, complete los campos necesarios: Nombre de Paciente y Numero de Expediente.")
+            return
+    try:
+        nombre = entry_nombre.get()
+        expediente = int(entry_expediente.get())
+        arbol_pacientes.Agregar(expediente, nombre)
+        actualizar_tabla()
+        entry_expediente.delete(0, tk.END)
+        entry_nombre.delete(0, tk.END)
+    except ValueError:
+        messagebox.showerror("Error", "Ingrese un número de expediente válido.")
+        return
 
 def buscar():
     expediente = int(entry_expediente.get())
@@ -109,20 +118,20 @@ def actualizar_tabla():
     _rellenar_tabla(arbol_pacientes.Raiz)
 
 def _rellenar_tabla(nodo):
-    if nodo:
+    if nodo:# Insertar el nodo actual en la tabla y continuar con los nodos izquierdo y derecho
         treeview.insert('', 'end', text=str(id(nodo)), values=(nodo.Nombre, nodo.Expediente))
-        _rellenar_tabla(nodo.Izquierda)
-        _rellenar_tabla(nodo.Derecha)
+        _rellenar_tabla(nodo.Izquierda)# Llamada recursiva para el nodo izquierdo
+        _rellenar_tabla(nodo.Derecha)#  Llamada recursiva para el nodo derecho
 
 #Parte 2 (AVL): Mantener la lista de doctores con inserciones y búsquedas rápidas. 
 #La interfaz debe permitir a los usuarios ver, agregar y buscar doctores.
 def agregar_doctor():
         # Validar que los campos de entrada no estén vacíos
         if not entry_id.get() or not entry_doctor.get():
-            print("Por favor, complete todos los campos.")
+            messagebox.showerror("Error", "Por favor, los campos necesarios: Nombre del Doctor y ID.")
             return
 
-        try:
+        try:# Obtener la clave y el nombre del doctor
             clave = int(entry_id.get())
             nombre = entry_doctor.get()
             # Agregar un doctor al árbol AVL
@@ -131,8 +140,8 @@ def agregar_doctor():
             actualizar_lista_doctores()
             # Limpiar los campos de entrada después de agregar
             entry_id.delete(0, tk.END)
-            entry_nombre.delete(0, tk.END)
-        except ValueError:
+            entry_doctor.delete(0, tk.END)
+        except ValueError:# Manejar errores de tipo de datos
             print("La clave debe ser un número entero.")
 
 def buscar_doctor():
@@ -142,13 +151,13 @@ def buscar_doctor():
             return
 
         try:
-            clave = int(entry_id.get())
+            clave = int(entry_id.get())# Obtener la clave del doctor a buscar
             # Buscar el doctor en el árbol AVL
             doctor =(arbol_avl.buscar(arbol_avl.raiz, clave))
             if doctor:
                 # Si se encuentra, mostrar la información del doctor en la interfaz
-                print(f"Doctor encontrado - Clave: {doctor.clave}, Nombre: {doctor.nombre}")
-                messagebox.showinfo("Resultado de Búsqueda", f"Doctor encontrado - Clave: {doctor.clave}, Nombre: {doctor.nombre}")
+                #print(f"Doctor encontrado - Clave: {doctor.clave}, Nombre: {doctor.nombre}")
+                messagebox.showinfo("Resultado de Búsqueda", f"EL Doctor ha sido encontrado \nID: {doctor.clave}, Nombre: {doctor.nombre}")
             else:
                 print("Doctor no encontrado")
         except ValueError:
@@ -163,25 +172,25 @@ def actualizar_lista_doctores():
         # Obtener la lista de doctores del árbol AVL y agregarlos a la lista de Tkinter
         doctores = arbol_avl.obtener_lista_doctores()
         for doctor in doctores:
-            treeview_doctores.insert("", "end", values=(doctor.clave, doctor.nombre))
+            treeview_doctores.insert("", "end", values=(doctor.nombre, doctor.clave))
 
 
 
 #Parte 3 (Grafo): Modelar la red de hospitales y encontrar la ruta más corta para la transferencia de pacientes. 
 #La interfaz debe permitir a los usuarios calcular y gestionar las transferencias de pacientes entre hospitales.
-def calcular_ruta():
-    inicio = entry_hospital_origen.get()
-    fin = entry_hospital_destino.get()
-    distancia_minima = grafo.Ruta_Mas_Corta(inicio, fin)
-    messagebox.showinfo("Ruta más corta", f"La distancia mínima entre {inicio} y {fin} es de {distancia_minima[1]} km.")
+def calcular_ruta():# Función para calcular la ruta más corta entre dos hospitales
+    inicio = entry_hospital_origen.get()# Obtener el hospital de origen
+    fin = entry_hospital_destino.get()# Obtener el hospital de destino
+    distancia_minima = grafo.Ruta_Mas_Corta(inicio, fin)# Calcular la ruta más corta
+    messagebox.showinfo("Ruta más corta", f"La distancia mínima entre {inicio} y {fin} es de {distancia_minima[1]} km.")# Mostrar la distancia mínima en una ventana emergente
 
-def transferir_paciente():
-    inicio = entry_hospital_origen.get()
-    fin = entry_hospital_destino.get()
-    nombre = entry_nombre_transf.get()
-    transferencia_ruta = grafo.Transferencia(inicio, fin)
-    messagebox.showinfo("Transferencia de Paciente",f" Se transfiere al paciente llamado: {nombre} desde el Origen: {inicio}  al Destino: {fin}, siguiendo la siguiente ruta: Ruta: {' -> '.join(transferencia_ruta[0])} con una distancia de {transferencia_ruta[1]} km" )
-
+def transferir_paciente():# Función para transferir un paciente de un hospital a otro
+    inicio = entry_hospital_origen.get()# Obtener el hospital de origen
+    fin = entry_hospital_destino.get()# Obtener el hospital de destino
+    nombre = entry_nombre_transf.get()# Obtener el nombre del paciente
+    transferencia_ruta = grafo.Transferencia(inicio, fin)# Calcular la ruta de transferencia
+    messagebox.showinfo("Transferencia de Paciente",f" Se transfiere al paciente llamado: {nombre} \nDesde el Origen: {inicio}  al Destino: {fin}\nSiguiendo la siguiente ruta: Ruta: {' -> '.join(transferencia_ruta[0])} con una distancia de {transferencia_ruta[1]} km" )
+    # Mostrar la ruta de transferencia en una ventana emergente
 
 # Botones para agregar, buscar y eliminar registros
 btn_agregar = tk.Button(window, text="Agregar Registro", command=agregar)
